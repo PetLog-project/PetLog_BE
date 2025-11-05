37f202036e05db07cfa8d6b4b7ba367086fc8c97
Hwangseoeun <sunny030921@gmail.com>
2025-11-05 18:55:46 +0900

    docs: 메시지 문구 변경

--- src/main/java/com/petlog/docs/MemberControllerDocs.java
+++ src/main/java/com/petlog/docs/MemberControllerDocs.java
@@ -2,14 +2,14 @@ package com.petlog.docs;
 
 import com.petlog.member.controller.dto.request.UpdateIsNotificationEnabledRequestDto;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import com.petlog.common.response.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
 @Tag(name = "회원 API")
 public interface MemberControllerDocs {
 
-    @ApiResponse(responseCode = "200", description = "알림 여부 설정에 성공했습니다.")
+    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "알림 여부 설정에 성공하였습니다.")
     @Operation(summary = "알림 수신 여부 설정 API")
-    ResponseEntity<com.petlog.common.response.ApiResponse<Void>> updateIsNotificationEnabled(UpdateIsNotificationEnabledRequestDto request);
+    ResponseEntity<ApiResponse<Void>> updateIsNotificationEnabled(UpdateIsNotificationEnabledRequestDto request);
 }
