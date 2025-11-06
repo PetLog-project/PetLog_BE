8558287c290037233fb63deec6551357eb543551
Hwangseoeun <sunny030921@gmail.com>
2025-11-06 18:04:19 +0900

    refactor: final 추가

--- src/main/java/com/petlog/docs/ScheduleControllerDocs.java
+++ src/main/java/com/petlog/docs/ScheduleControllerDocs.java
@@ -18,13 +18,13 @@ public interface ScheduleControllerDocs {
 
     @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "월별 일정 전체 조회에 성공하였습니다.")
     @Operation(summary = "월별 일정 전체 조회 API")
-    ResponseEntity<ApiResponse<GetMonthlyScheduleResponseDto>> getAllSchedule(@PathVariable Long groupId, @RequestParam @DateTimeFormat(pattern = "yyyy-MM") YearMonth date);
+    ResponseEntity<ApiResponse<GetMonthlyScheduleResponseDto>> getAllSchedule(@PathVariable final Long groupId, @RequestParam @DateTimeFormat(pattern = "yyyy-MM") final YearMonth date);
 
     @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "일정 상세 내용 수정에 성공하였습니다.")
     @Operation(summary = "일정 상세 내용 수정 API")
-    ResponseEntity<ApiResponse<Void>> updateSchedule(@PathVariable Long groupId, @PathVariable Long scheduleId, @RequestBody UpdateScheduleRequestDto request);
+    ResponseEntity<ApiResponse<Void>> updateSchedule(@PathVariable final Long groupId, @PathVariable final Long scheduleId, @RequestBody final UpdateScheduleRequestDto request);
 
     @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "일정 삭제에 성공하였습니다.")
     @Operation(summary = "일정 삭제 API")
-    ResponseEntity<ApiResponse<Void>> deleteSchedule(@PathVariable Long groupId, @PathVariable Long scheduleId);
+    ResponseEntity<ApiResponse<Void>> deleteSchedule(@PathVariable final Long groupId, @PathVariable final Long scheduleId);
 }
