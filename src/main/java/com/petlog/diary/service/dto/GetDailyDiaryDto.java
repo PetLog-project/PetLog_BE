a6058a764ded1b1bda9b6747c5ca2a925ddf2f70
Hwangseoeun <sunny030921@gmail.com>
2025-11-30 16:02:30 +0900

    refactor: 불필요한 생성자 제거

--- src/main/java/com/petlog/diary/service/dto/GetDailyDiaryDto.java
+++ src/main/java/com/petlog/diary/service/dto/GetDailyDiaryDto.java
@@ -1,19 +1,15 @@
 package com.petlog.diary.service.dto;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDate;
 import java.util.List;
 
 public record GetDailyDiaryDto(
 
+    @DateTimeFormat(pattern = "yyyy.MM.dd")
     LocalDate writtenAt,
     List<GetDiaryInfoDto> diaryInfo
 
 ) {
-    public GetDailyDiaryDto(
-        final LocalDate writtenAt,
-        final List<GetDiaryInfoDto> diaryInfo
-    ) {
-        this.writtenAt = writtenAt;
-        this.diaryInfo = diaryInfo;
-    }
 }
